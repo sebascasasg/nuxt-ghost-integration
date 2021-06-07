4708c426861523ab9050ad254bcb9c59a3f42399
sebascasasg <sebascasas.developer@gmail.com>
2021-06-07 12:00:16 -0500

    Added content and styles to Home

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <div></div>
+  <div class="container">
+    <h1 class="title">Parque Explora</h1>
+    <p class="snippet">
+      Este es el Home del sitio. Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Quas maiores, minima, nesciunt quam ab minus blanditiis
+      corrupti cumque laboriosam in consequatur, magni quod voluptates
+      praesentium ut accusamus distinctio earum nisi assumenda illum suscipit
+      sint rerum! Beatae adipisci exercitationem sint veritatis a possimus
+      harum. Aut illo modi doloribus quaerat rem iusto amet minus, suscipit quia
+      molestias quae fuga ut ea repellat ullam eos architecto qui et consequatur
+      expedita reiciendis error! Doloremque quam voluptatum delectus sed
+      sapiente fugiat nobis corporis facere dolore. Alias, adipisci et
+      necessitatibus laborum recusandae impedit placeat accusamus quaerat
+      repellendus dolorum distinctio perferendis est velit consequatur
+      doloremque dolor quisquam!
+    </p>
+  </div>
 </template>
 
 <script>
@@ -7,4 +23,13 @@ export default {};
 </script>
 
 <style scoped>
+.title {
+  margin: 2rem 0;
+  font-size: 5rem;
+  font-weight: bold;
+  text-align: center;
+}
+.snippet {
+  margin: 0 auto 4rem;
+}
 </style>
