ddc49fa9aef87edc946c119cae65f918ba5c662f
sebascasasg <sebascasas.developer@gmail.com>
2021-06-07 11:59:39 -0500

    Created page for "Conocenos" section

--- pages/conocenos.vue
+++ pages/conocenos.vue
@@ -1,10 +1,25 @@
 <template>
-  <div></div>
+  <main class="container">
+    <h1 class="title">{{ page.title }}</h1>
+    <article v-html="page.html" />
+  </main>
 </template>
 
 <script>
-export default {};
+import { getSinglePage } from "~/api/pages";
+export default {
+  async asyncData() {
+    const page = await getSinglePage("conocenos");
+    return { page: page };
+  },
+};
 </script>
 
 <style scoped>
+.title {
+  margin: 2rem 0;
+  font-size: 5rem;
+  font-weight: bold;
+  text-align: center;
+}
 </style>
